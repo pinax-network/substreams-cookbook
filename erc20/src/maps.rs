043e4e281b7042638bc7a6fe6c26d7509f8472e2
Denis Carriere <carriere.denis@gmail.com>
2023-06-16 14:42:37 -0400

    add logging

--- erc20/src/maps.rs
+++ erc20/src/maps.rs
@@ -2,13 +2,21 @@ use substreams::Hex;
 use substreams::log;
 use substreams::errors::Error;
 use substreams_ethereum::pb::eth::v2::Block;
-use crate::erc20::{TransferEvents};
+use crate::erc20::{TransferEvent,TransferEvents};
 
 #[substreams::handlers::map]
 pub fn map_transfers(block: Block) -> Result<TransferEvents, Error> {
     let mut events = vec![];
+    log::info!("events: {:?}", events);
     for trace in block.transaction_traces.clone() {
-        log::info!("trace: {:?}", trace);
+        for calls in trace.calls {
+            let address = Hex(calls.address).to_string();
+            log::info!("call.address: {:?}", address);
+            events.push(TransferEvent{
+                from: address.clone(),
+                to: address,
+            });
+        }
     }
     Ok(TransferEvents {events})
 }
