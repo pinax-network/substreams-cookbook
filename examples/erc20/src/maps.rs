4b3f33f7d4a561928c05c94e6f0f4b96d8b07fa2
Denis Carriere <carriere.denis@gmail.com>
2023-06-19 15:43:20 -0400

    refactor: Hex::encode

--- examples/erc20/src/maps.rs
+++ examples/erc20/src/maps.rs
@@ -8,24 +8,25 @@ use crate::pb::erc20::types::v1::{TransferEvent, TransferEvents, ApprovalEvent, 
 // Token contract addresses
 pub const TETHER: [u8; 20] = hex!("dac17f958d2ee523a2206206994597c13d831ec7");
 pub const USDC: [u8; 20] = hex!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+pub const ADDRESSES: &[&[u8]] = &[&TETHER, &USDC];
 
 #[substreams::handlers::map]
 pub fn map_transfer(block: Block) -> Result<TransferEvents, Error> {
     Ok(TransferEvents {
         events: block
-            .events::<Transfer>(&[&TETHER, &USDC])
+            .events::<Transfer>(&ADDRESSES)
             .filter_map(|(event, log)| {
                 Some(TransferEvent {
                     // contract address
-                    address: Hex(log.address()).to_string(),
+                    address: Hex::encode(log.address()),
 
                     // event payload
-                    from: Hex(event.from).to_string(),
-                    to: Hex(event.to).to_string(),
+                    from: Hex::encode(event.from),
+                    to: Hex::encode(event.to),
                     value: event.value.to_string(),
 
                     // trace information
-                    transaction: Hex(&log.receipt.transaction.hash).to_string(),
+                    transaction: Hex::encode(&log.receipt.transaction.hash),
                 })
             })
             .collect()
@@ -36,25 +37,24 @@ pub fn map_transfer(block: Block) -> Result<TransferEvents, Error> {
 pub fn map_approval(block: Block) -> Result<ApprovalEvents, Error> {
     Ok(ApprovalEvents {
         events: block
-            .events::<Approval>(&[&TETHER, &USDC])
+            .events::<Approval>(&ADDRESSES)
             .filter_map(|(event, log)| {
                 Some(ApprovalEvent {
                     // contract address
-                    address: Hex(log.clone().address()).to_string(),
+                    address: Hex::encode(log.clone().address()),
 
                     // event payload
-                    owner: Hex(event.owner).to_string(),
-                    spender: Hex(event.spender).to_string(),
+                    owner: Hex::encode(event.owner),
+                    spender: Hex::encode(event.spender),
                     value: event.value.to_string(),
 
                     // trace information
-                    transaction: Hex(&log.receipt.transaction.hash).to_string(),
+                    transaction: Hex::encode(&log.receipt.transaction.hash),
                 })
             })
             .collect()
     })
 }
-
 
 #[substreams::handlers::map]
 pub fn map_balance_of(block: Block) -> Result<BalanceOfStorageChanges, Error> {
@@ -64,19 +64,19 @@ pub fn map_balance_of(block: Block) -> Result<BalanceOfStorageChanges, Error> {
     for calls in block.calls() {
         // Storage changes
         for storage_change in &calls.call.storage_changes {
-            let address = Hex(&storage_change.address).to_string();
+            let address = Hex::encode(&storage_change.address);
             if address != "dac17f958d2ee523a2206206994597c13d831ec7" { continue; }
 
             storage_changes.push(BalanceOfStorageChange {
                 // contract address
-                address: address.to_string(),
+                address,
 
                 // storage changes
-                owner: Hex(&calls.call.caller).to_string(),
-                balance: Hex(&storage_change.new_value).to_string(),
+                owner: Hex::encode(&calls.call.caller),
+                balance: Hex::encode(&storage_change.new_value),
 
                 // trace information
-                transaction: Hex(&calls.transaction.hash).to_string(),
+                transaction: Hex::encode(&calls.transaction.hash),
             })
         }
     }
