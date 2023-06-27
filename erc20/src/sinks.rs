1f2531b20fb2872d7aef8505ffece814b0a8e322
Denis Carriere <carriere.denis@gmail.com>
2023-06-27 11:02:55 -0400

    add clock

--- erc20/src/sinks.rs
+++ erc20/src/sinks.rs
@@ -1,11 +1,13 @@
-use substreams::errors::Error;
+use substreams::{errors::Error, pb::substreams::Clock};
 use substreams_entity_change::tables::Tables;
 use substreams_entity_change::pb::entity::EntityChanges;
 use crate::pb::erc20::types::v1::{TransferEvents, ApprovalEvents, BalanceOfStorageChanges};
 
 #[substreams::handlers::map]
-pub fn graph_out(map_transfer: TransferEvents, map_approval: ApprovalEvents, map_balance_of: BalanceOfStorageChanges) -> Result<EntityChanges, Error> {
+pub fn graph_out(clock: Clock, map_transfer: TransferEvents, map_approval: ApprovalEvents, map_balance_of: BalanceOfStorageChanges) -> Result<EntityChanges, Error> {
     let mut tables = Tables::new();
+    let block_num = clock.number.to_string();
+    let timestamp = clock.timestamp.unwrap().seconds.to_string();
 
     for event in map_transfer.events {
         let id = format!("{}-{}", event.block_index, event.transaction);
@@ -21,7 +23,9 @@ pub fn graph_out(map_transfer: TransferEvents, map_approval: ApprovalEvents, map
             .set("value", event.value)
 
             // trace information
-            .set("transaction", event.transaction);
+            .set("transaction", event.transaction)
+            .set_bigint("block_num", &block_num)
+            .set_bigint("timestamp", &timestamp);
     }
 
     for event in map_approval.events {
@@ -38,7 +42,9 @@ pub fn graph_out(map_transfer: TransferEvents, map_approval: ApprovalEvents, map
             .set("value", event.value)
 
             // trace information
-            .set("transaction", event.transaction);
+            .set("transaction", event.transaction)
+            .set_bigint("block_num", &block_num)
+            .set_bigint("timestamp", &timestamp);
     }
 
     for storage_change in map_balance_of.storage_changes {
@@ -55,7 +61,9 @@ pub fn graph_out(map_transfer: TransferEvents, map_approval: ApprovalEvents, map
             .set("balance", storage_change.balance)
 
             // trace information
-            .set("transaction", storage_change.transaction);
+            .set("transaction", storage_change.transaction)
+            .set_bigint("block_num", &block_num)
+            .set_bigint("timestamp", &timestamp);
     }
 
     Ok(tables.to_entity_changes())
